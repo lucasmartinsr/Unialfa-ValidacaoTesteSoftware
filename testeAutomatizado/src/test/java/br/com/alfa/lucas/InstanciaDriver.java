f2da1be318b0e2973356682aa8dca0f3a7b9c6f9
Lucas Martins <lucas_mr92@hotmail.com>
2019-05-04 15:13:21 -0300

    Criando teste automatizado.

---
+++ testeAutomatizado/src/test/java/br/com/alfa/lucas/InstanciaDriver.java
@@ -0,0 +1,25 @@
+package br.com.alfa.lucas;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InstanciaDriver {
+
+    public static WebDriver getDriver(){
+
+        WebDriver driver = null;
+
+        final String OS = System.getProperty("os.name").toLowerCase();
+
+        String urlDriver = OS.contains("win")? "src/test/resources/chromedriver.exe": "src/test/resources/chromedriver";
+
+        System.setProperty("webdriver.chrome.driver", urlDriver);
+
+        try{
+            driver = new ChromeDriver();
+        }catch(Exception e){
+            System.out.println("Erro ao instanciar o driver");
+        }
+        return driver;
+    }
+}
